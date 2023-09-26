1e204aa31a982a37ceff9cbb4d3e1079ffbd8a42
Pluto-the-Planet9 <camerondj2007@outlook.com>
2023-09-26 09:10:51 -0400

    task 2

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+calculate_area_length = input("What is the length?: ")
+calculate_area_width = input("What is the width ?: ")
+calculate_area_length = float(calculate_area_length)
+calculate_area_width = float(calculate_area_width)
+print("The parameter is:")
+print(calculate_area_length + calculate_area_width)
+print("The area is:")
+print(calculate_area_length * calculate_area_width)
