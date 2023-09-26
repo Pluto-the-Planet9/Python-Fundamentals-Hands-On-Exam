ab01dc34f1cff9d96863f43de72012ec71ea2789
Pluto-the-Planet9 <camerondj2007@outlook.com>
2023-09-26 09:03:46 -0400

    task 1

---
+++ Task 1.py
@@ -0,0 +1,23 @@
+intro = "Hello World"
+print(intro)
+
+print("Hello"  +  "World")
+
+first_name = input("What is your first name?: ")
+print(first_name)
+
+age = input("What is your age?: ")
+age = int(age)
+temperature = input("What is the temperature?: ")
+temperature = float(temperature)
+too_hot = input("Is it hot or cold?: ")
+
+print(age)
+print(temperature)
+print(too_hot)
+
+print(age + temperature)
+print(age - temperature)
+print(age * temperature)
+print(age / temperature)
+print(age // temperature)
