f30f051f4982ce38bd541e6c0dbf3899174de19f
Pluto-the-Planet9 <camerondj2007@outlook.com>
2023-09-26 09:16:26 -0400

    task 3

---
+++ Task 3.py
@@ -0,0 +1,9 @@
+number = input("What is your number?: ")
+number = int(number)
+
+if number > 0:
+  print("It is a posative number")
+elif number == 0:
+  print("The number is 0")
+else:
+  print("It is a negitive number")
